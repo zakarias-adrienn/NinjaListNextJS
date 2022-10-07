e2aaba3600be50a3870adb45b526b67692ea0781
Zakariás Adrienn <zakarias.adrienn@yahoo.com>
2022-10-07 09:19:42 +0300

    initial

---
+++ pages/about.js
@@ -0,0 +1,26 @@
+import Head from "next/head";
+
+const About = () => {
+  return (
+    <>
+      <Head>
+        <title>Ninja List | About</title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
+      <div>
+        <h1>About</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus quis
+          bibendum quam. Orci varius natoque penatibus et magnis dis parturient
+          montes, nascetur ridiculus mus. Cras id dui quis mauris tincidunt
+          semper eget quis turpis. Sed vitae feugiat eros, semper sagittis
+          libero. Ut efficitur nunc eu mauris imperdiet, non cursus turpis
+          vehicula. Cras pellentesque tempor nisi eget interdum. Morbi aliquam
+          tincidunt risus in volutpat.
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default About;
